506a344fb5700418e96da3f42b5cc3b2643961c8
shamith-perera <shamith.gsjp@gmail.com>
2024-06-21 11:48:45 +0530

    added some ui channges with css

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ function calSum() {
     let num1 = new Number(document.getElementById("number1").value);
     let num2 = new Number(document.getElementById("number2").value);
     let operator = document.getElementById("operator").value;
-    let answerLabel = document.getElementById("answer");
+    let answerDisplay = document.getElementById("answer");
     switch(operator){
-        case "+": answerLabel.innerHTML = num1 + num2; break;
-        case "-": answerLabel.innerHTML = num1 - num2; break;
-        case "*": answerLabel.innerHTML = num1 * num2; break;
-        case "/": answerLabel.innerHTML = num1 / num2; break;
+        case "+": answerDisplay.value = num1 + num2; break;
+        case "-": answerDisplay.value = num1 - num2; break;
+        case "*": answerDisplay.value = num1 * num2; break;
+        case "/": answerDisplay.value = num1 / num2; break;
         default : alert("Invalid Operator");
     }
 }
